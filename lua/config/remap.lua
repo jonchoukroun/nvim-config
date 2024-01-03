cd4fe72b55299bcd8ff1fd6688164d1df9a43e69
Jon Choukroun <jonchoukroun@gmail.com>
2024-01-03 10:08:54 -0800

    Remap scroll to center position

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -12,3 +12,7 @@ vim.keymap.set("n", "<Up>", "<cmd>resize +2<CR>", { noremap = true, silent = tru
 vim.keymap.set("n", "<Down>", "<cmd>resize -2<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Left>", "<cmd>vertical resize +2<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Right>", "<cmd>vertical resize -2<CR>", { noremap = true, silent = true })
+
+-- Scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
