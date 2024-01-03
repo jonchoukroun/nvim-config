c7b56607d4e3739f51a803c7d23cdfa62fe803a9
Jon Choukroun <jonchoukroun@gmail.com>
2024-01-03 09:43:10 -0800

    Add highlight clear, alt file, and resize remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,14 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>rw", vim.cmd.Ex, { desc = "Return to NetRw" })
+
+-- Clear search highlighting
+vim.keymap.set("n", "<leader>/", "<cmd>noh<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader><Tab>", "<C-^>", { desc = "Alt File" })
+
+-- Resizing
+vim.keymap.set("n", "<Up>", "<cmd>resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", "<cmd>resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", "<cmd>vertical resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", "<cmd>vertical resize -2<CR>", { noremap = true, silent = true })
