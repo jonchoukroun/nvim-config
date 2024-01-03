8fea6f5f906fb42a42535aa917564ef135552c91
Jon Choukroun <jonchoukroun@gmail.com>
2024-01-03 06:19:59 -0800

    Basic neovim config

---
+++ lua/config/core.lua
@@ -0,0 +1,14 @@
+local opt = vim.opt
+
+-- Statusline
+opt.laststatus = 3
+
+-- Relative and Absolute numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Set tab and indent to 4 spaces
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.smartindent = true
---
+++ lua/plugins/commenter.lua
@@ -0,0 +1,6 @@
+return {
+    'preservim/nerdcommenter',
+    config = function()
+        vim.g.NERDSpaceDelims = 1
+    end
+}
