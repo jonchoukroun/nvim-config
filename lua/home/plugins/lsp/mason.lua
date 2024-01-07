590ab9f9511e27de4f12e56fa15abaac5442567a
Jon Choukroun <jonchoukroun@gmail.com>
2024-01-06 19:57:24 -0800

    Set up nvim-lint and mason tools installer

--- lua/home/plugins/lsp/mason.lua
+++ lua/home/plugins/lsp/mason.lua
@@ -2,10 +2,12 @@ return {
     "williamboman/mason.nvim",
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
         mason.setup({
             ui = {
@@ -25,6 +27,14 @@ return {
                 "lua_ls",
                 "emmet_ls",
             },
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "eslint_d",
+                "prettierd",
+                "stylua",
+            }
         })
     end
 }
