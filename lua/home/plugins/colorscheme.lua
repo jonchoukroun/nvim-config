f60689db7997103395ed41ce17cb734bfdbe912a
Jon Choukroun <jonchoukroun@gmail.com>
2024-02-03 13:40:03 -0800

    Update colorscheme.lua

--- lua/home/plugins/colorscheme.lua
+++ lua/home/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-	'morhetz/gruvbox',
+	-- "morhetz/gruvbox",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.opt.termguicolors = true
-		vim.cmd [[colorscheme gruvbox]]
-	end
+		-- vim.cmd([[colorscheme gruvbox]])
+		vim.cmd([[colorscheme catppuccin]])
+	end,
 }
