4d5ddf37f7192cad832a95117f0cf4f301d624f0
Jon Choukroun <jonchoukroun@gmail.com>
2024-02-03 13:44:10 -0800

    Update lspconfig.lua

--- lua/home/plugins/lspconfig.lua
+++ lua/home/plugins/lspconfig.lua
@@ -1,58 +1,79 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
+	{
+
 		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		{
-			"nvimdev/lspsaga.nvim",
-			config = function()
-				require("lspsaga").setup({})
-			end,
-		},
+		config = function()
+			require("mason").setup()
+		end,
 	},
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "rust_analyzer", "tsserver" },
-		})
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				auto_install = true,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			-- Diagnostics
+			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-		-- Diagnostics
-		vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local opts = { desc = "", buffer = ev.buf }
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local opts = { desc = "", buffer = ev.buf }
+					opts.desc = "Go to definition"
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 
-				opts.desc = "Go to definition"
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					opts.desc = "Go to declaration"
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-				opts.desc = "Go to declaration"
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					opts.desc = "Go to implementation"
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 
-				opts.desc = "Go to implementation"
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					opts.desc = "Go to references"
+					vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
 
-				opts.desc = "Go to references"
-				vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+					opts.desc = ""
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-				opts.desc = ""
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					opts.desc = "Rename"
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				end,
+			})
 
-				opts.desc = "Rename"
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-			end,
-		})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		-- Go to
-
-		require("lspconfig").lua_ls.setup({})
-		require("lspconfig").rust_analyzer.setup({})
-		require("lspconfig").tsserver.setup({})
-	end,
+			require("lspconfig").lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+			require("lspconfig").tsserver.setup({
+				capabilities = capabilities,
+			})
+			require("lspconfig").clangd.setup({
+				capabilities = capabilities,
+			})
+			require("lspconfig").sourcekit.setup({
+				capabilities = capabilities,
+				cmd = { "/usr/bin/sourcekit-lsp" },
+				root_dir = vim.fs.dirname(vim.fs.find({ "*.swift" }, { upward = true })[0]),
+			})
+		end,
+	},
 }
