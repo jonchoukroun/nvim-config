2627fdfab64a98bdaf7045a9789d2fa9dc06709a
Jon Choukroun <jonchoukroun@gmail.com>
2024-02-03 13:42:12 -0800

    Update illuminate.lua

--- lua/home/plugins/illuminate.lua
+++ lua/home/plugins/illuminate.lua
@@ -9,16 +9,16 @@ return {
         },
     },
     config = function(_, opts)
-    require("illuminate").configure(opts)
+        require("illuminate").configure(opts)
 
-    local function map(key, dir, buffer)
-      vim.keymap.set("n", key, function()
-        require("illuminate")["goto_" .. dir .. "_reference"](false)
-      end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
-    end
+        local function map(key, dir, buffer)
+            vim.keymap.set("n", key, function()
+                require("illuminate")["goto_" .. dir .. "_reference"](false)
+            end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+        end
 
-    map("]]", "next")
-    map("[[", "prev")
+        map("]]", "next")
+        map("[[", "prev")
 
         -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
         vim.api.nvim_create_autocmd("FileType", {
