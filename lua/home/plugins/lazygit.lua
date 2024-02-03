e70ab065eca495bb00569c21ac6b53bd75bfbac1
Jon Choukroun <jonchoukroun@gmail.com>
2024-02-03 13:43:30 -0800

    Update lazygit.lua

--- lua/home/plugins/lazygit.lua
+++ lua/home/plugins/lazygit.lua
@@ -1,9 +1,13 @@
 return {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    keys = {
-        {"<leader>gg", "<cmd>LazyGit<CR>", desc = "Lazy Git"}
-    }
+	"kdheepak/lazygit.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{ "<leader>gg", "<cmd>LazyGit<CR>", desc = "Lazy Git" },
+	},
+	config = function()
+		require("telescope").load_extension("lazygit")
+	end,
 }
