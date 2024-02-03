23f77798c7b908c9148c3dd50c7b8b94c38420c9
Jon Choukroun <jonchoukroun@gmail.com>
2024-02-03 13:48:07 -0800

    Update treesitter.lua

--- lua/home/plugins/treesitter.lua
+++ lua/home/plugins/treesitter.lua
@@ -1,29 +1,41 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"windwp/nvim-ts-autotag",
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		"nvim-treesitter/nvim-treesitter-context",
-	},
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        "windwp/nvim-ts-autotag",
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        "nvim-treesitter/nvim-treesitter-context",
+    },
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        require("nvim-ts-autotag").setup()
 
-		configs.setup({
-			ensure_installed = { "lua", "rust", "markdown" },
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = { enable = true },
-		})
+        local configs = require("nvim-treesitter.configs")
 
-		require("ts_context_commentstring").setup({})
+        configs.setup({
+            ensure_installed = {
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "objc",
+                "rust",
+                "swift",
+                "typescript",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotag = { enable = true },
+        })
 
-		require("treesitter-context").setup({
-			multiline_threshold = 4,
-			separator = "_",
-		})
-	end,
+        require("ts_context_commentstring").setup({})
+
+        require("treesitter-context").setup({
+            multiline_threshold = 4,
+            separator = "_",
+        })
+    end,
 }
