983fe2f87a9c0c6f43ab0a62f81d9e372e8b3ec8
Jon Choukroun <jonchoukroun@gmail.com>
2024-02-03 13:38:27 -0800

    Update keymaps.lua

--- lua/home/core/keymaps.lua
+++ lua/home/core/keymaps.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = " "
 vim.g.localmapleader = " "
 
-vim.keymap.set("n", "<leader>rw", vim.cmd.Ex, { desc = "Return to NetRw" })
+-- We're using Oil.nvim instead of NetRw for now
+-- vim.keymap.set("n", "<leader>rw", vim.cmd.Ex, { desc = "Return to NetRw" })
 
 -- Clear search highlighting
 vim.keymap.set("n", "<esc>", "<cmd>noh<CR>", { noremap = true, silent = true })
@@ -17,3 +18,6 @@ vim.keymap.set("n", "<Right>", "<cmd>vertical resize -2<CR>", { noremap = true, 
 -- Center when scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+
+vim.keymap.set("n", "]b", "<cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "[b", "<cmd>bprev<CR>", { desc = "Previous buffer" })
