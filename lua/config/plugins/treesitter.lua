5c2f9de01bdc92484957b32242ee8442d414998f
Jon Choukroun <jonchoukroun@gmail.com>
2024-03-09 14:50:35 -0800

    Match treesitter config

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,41 +1,43 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        "windwp/nvim-ts-autotag",
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        "nvim-treesitter/nvim-treesitter-context",
-    },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require("nvim-ts-autotag").setup()
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		"nvim-treesitter/nvim-treesitter-context",
+		"windwp/nvim-ts-autotag",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"cpp",
+				"css",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"objc",
+				"rust",
+				"tsx",
+				"typescript",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			autotag = { enable = true },
+		})
 
-        configs.setup({
-            ensure_installed = {
-                "css",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "rust",
-                "typescript",
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            autotag = { enable = true },
-        })
+		require("ts_context_commentstring").setup({})
 
-        require("ts_context_commentstring").setup({})
-
-        require("treesitter-context").setup({
-            multiline_threshold = 4,
-            separator = "_",
-        })
-    end,
+		require("treesitter-context").setup({
+			multiline_threshold = 4,
+			separator = "_",
+		})
+	end,
 }
