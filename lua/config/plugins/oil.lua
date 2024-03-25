c6f7c6befbdb6f589d43697f05cdbcf1d587d474
Jon Choukroun <jonchoukroun@gmail.com>
2024-03-25 15:39:39 -0700

    Temp disable oil while evaluating mini.files

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -1,9 +1,9 @@
 return {
-    'stevearc/oil.nvim',
-    opts = {},
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function ()
-        require("oil").setup()
-        vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory (Oil)" })
-    end
+	-- 'stevearc/oil.nvim',
+	-- opts = {},
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- config = function ()
+	--     require("oil").setup()
+	--     vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory (Oil)" })
+	-- end
 }
