ea8cba30dc332a27d476bd4e6c0c21bdb15d5743
Jon Choukroun <jonchoukroun@gmail.com>
2024-03-31 15:06:17 -0700

    Customize notify rendering

--- lua/config/plugins/nvim-notify.lua
+++ lua/config/plugins/nvim-notify.lua
@@ -1,6 +1,10 @@
 return {
 	"rcarriga/nvim-notify",
 	config = function()
-		require("notify").setup()
+		require("notify").setup({
+			stages = "static",
+			render = "wrapped-compact",
+			top_down = false,
+		})
 	end,
 }
