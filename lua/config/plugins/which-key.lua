48a38e2fcdb95ea72741aa2cac73172c217c9557
Jon Choukroun <jonchoukroun@gmail.com>
2024-03-31 21:16:58 -0700

    Improve whichkey UI

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -1,11 +1,16 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-    end,
-    config = function()
-        require("which-key").setup()
-    end
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	config = function()
+		require("which-key").setup({
+			window = {
+				border = "shadow",
+				margin = { 0, 5, 2, 5 },
+			},
+		})
+	end,
 }
