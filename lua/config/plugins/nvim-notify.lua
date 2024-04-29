3ed39d998a72311bb456612e7b1e598b3de00ebe
Jon Choukroun <jonchoukroun@gmail.com>
2024-04-29 10:52:24 -0700

    Remove custom borland UI

--- lua/config/plugins/nvim-notify.lua
+++ lua/config/plugins/nvim-notify.lua
@@ -1,17 +1,10 @@
 return {
 	"rcarriga/nvim-notify",
 	config = function()
-		local on_open = function(win)
-			local conf = vim.api.nvim_win_get_config(win)
-			conf.border = "shadow"
-			vim.api.nvim_win_set_config(win, conf)
-		end
-
 		require("notify").setup({
 			stages = "static",
 			render = "wrapped-compact",
 			top_down = false,
-			on_open = on_open,
 		})
 	end,
 }
