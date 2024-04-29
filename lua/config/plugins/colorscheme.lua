d5673d4d2df8d2cb570cefb57848e42f3f71f6a4
Jon Choukroun <jonchoukroun@gmail.com>
2024-04-29 10:49:20 -0700

    Use pleasant kanagawa colorscheme

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	"jonchoukroun/borland-colors",
+	"rebelot/kanagawa.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.opt.termguicolors = true
-		vim.cmd([[colorscheme borland]])
+		vim.cmd([[colorscheme kanagawa]])
 	end,
 }
