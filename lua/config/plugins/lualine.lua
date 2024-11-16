5cff678b8fca029d709bdc6c0e294f6034032e53
Jon Choukroun <jonchoukroun@gmail.com>
2024-11-15 16:53:36 -0800

    Configure with custom theme

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -1,3 +1,26 @@
+local theme = {
+	normal = {
+		a = "HighlightPrimary",
+		b = "PrimaryText",
+		c = "HiddenText",
+		x = "HiddenText",
+		y = "HiddenText",
+		z = "HiddenText",
+	},
+	insert = {
+		a = "HighlightAccent",
+		z = "HiddenText",
+	},
+	visual = {
+		a = "HighlightCallout",
+		z = "HiddenText",
+	},
+	replace = {
+		a = "HighlightError",
+		z = "HiddenText",
+	},
+}
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -6,12 +29,15 @@ return {
 			options = {
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "", right = "" },
+				theme = theme,
 			},
 			sections = {
-				lualine_y = {
+				lualine_b = {
+					"branch",
+					"diff",
 					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
+						"diagnostics",
+						sources = { "nvim_diagnostic", "nvim_lsp" },
 					},
 				},
 			},
