976d6d9f1d1b358983f6eca4986908e67e10069b
Jon Choukroun <jonchoukroun@gmail.com>
2024-12-22 15:12:12 -0800

    Configure blink for completions

---
+++ lua/config/plugins/blink.lua
@@ -0,0 +1,87 @@
+---@diagnostic disable: missing-fields
+return {
+	"saghen/blink.cmp",
+	lazy = false,
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		{ "L3MON4D3/LuaSnip", version = "v2.*" },
+	},
+	version = "v0.*",
+	---@module "blink.cmp"
+	---@type blink.cmp.Config
+	opts = {
+		completion = {
+			accept = {
+				auto_brackets = {
+					enabled = true,
+				},
+			},
+			menu = {
+				border = "single",
+				draw = {
+					columns = { { "kind_icon" }, { "label", "label_description", "kind", gap = 2 } },
+					components = {},
+				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				window = {
+					border = "single",
+				},
+			},
+		},
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+			kind_icons = {
+				Text = "",
+				Method = "",
+				Function = "",
+				Constructor = "",
+				Field = "",
+				Variable = "",
+				Class = "",
+				Interface = "",
+				Module = "",
+				Property = "",
+				Unit = "",
+				Value = "",
+				Enum = "",
+				Keyword = "",
+				Snippet = "",
+				Color = "",
+				File = "",
+				Reference = "",
+				Folder = "",
+				EnumMember = "",
+				Constant = "",
+				Struct = "",
+				Event = "",
+				Operator = "",
+				TypeParameter = "",
+			},
+		},
+		keymap = {
+			preset = "default",
+			["<C=space>"] = {},
+			["<C-e>"] = {},
+			["<C-s>"] = { "show_documentation", "hide_documentation" },
+			["<C-x>"] = { "show", "hide" },
+		},
+		snippets = {
+			expand = function(snippet)
+				require("luasnip").lsp_expand(snippet)
+			end,
+			active = function(filter)
+				if filter and filter.direction then
+					return require("luasnip").jumpable(filter.direction)
+				end
+				return require("luasnip").in_snippet()
+			end,
+			jump = function(direction)
+				require("luasnip").jump(direction)
+			end,
+		},
+	},
+}
