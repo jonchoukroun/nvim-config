7048358f7f8ea88cdc5fe6f84d86f6083557e475
Jon Choukroun <jonchoukroun@gmail.com>
2025-01-27 13:01:46 -0800

    Disable color scheme

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-	"jonchoukroun/retro-bluish",
-	-- "rebelot/kanagawa.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.opt.termguicolors = true
-		vim.cmd("colorscheme retro-bluish")
-	end,
+	-- "jonchoukroun/retro-bluish",
+	-- -- "rebelot/kanagawa.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.opt.termguicolors = true
+	-- 	vim.cmd("colorscheme retro-bluish")
+	-- end,
 }
