68336104b3a2495ecf5a06da4bd12cf8ffddcbe8
Jon Choukroun <jonchoukroun@gmail.com>
2025-02-03 22:18:11 -0800

    Configure c, cpp debugging

--- lua/config/plugins/nvim-dap.lua
+++ lua/config/plugins/nvim-dap.lua
@@ -1,17 +1,54 @@
 return {
-	"jay-babu/mason-nvim-dap.nvim",
+	"mfussenegger/nvim-dap",
 	dependencies = {
 		"williamboman/mason.nvim",
-		"mfussenegger/nvim-dap",
+		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-neotest/nvim-nio",
 	},
+	config = function()
+		require("nvim-dap-virtual-text").setup()
+
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "/Applications/Xcode.app/Contents/Developer/usr/bin/lldb-dap",
+			name = "lldb",
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+		}
+		dap.configurations.c = dap.configurations.cpp
+
+		dapui.setup()
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
 	event = "VeryLazy",
-	opts = {
-		handlers = {},
-		ensure_installed = { "codelldb" },
-	},
 	keys = {
-		{ "<leader>db", "<cmd>DapToggleBreakpoint<CR>", desc = "Toggle Breakpoint" },
-		{ "<leader>dr", "<cmd>DapContinue<CR>", desc = "Run Debugger" },
-		{ "<leader>ds", "<cmd>DapTerminate<CR>", desc = "Stop Debugger" },
+		{ "<leader>db", "<cmd>DapToggleBreakpoint<CR>", desc = "DAP: Toggle Breakpoint" },
+		{ "<leader>dr", "<cmd>DapContinue<CR>", desc = "DAP: Run" },
+		{ "<leader>ds", "<cmd>DapTerminate<CR>", desc = "DAP: Terminate" },
 	},
 }
