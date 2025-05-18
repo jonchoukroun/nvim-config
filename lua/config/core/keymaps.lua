cf751b5d4c9091feefb0c4a6c19214ca4a7e9e62
Jon Choukroun <jonchoukroun@gmail.com>
2025-05-17 22:16:53 -0700

    Fix buffer management keymap organization

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -16,9 +16,8 @@ vim.keymap.set("n", "<Right>", "<cmd>vertical resize -2<CR>", { noremap = true, 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 
+-- Buffer management
 vim.keymap.set("n", "]b", "<cmd>bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "[b", "<cmd>bprev<CR>", { desc = "Previous buffer" })
-
--- Buffer management
 vim.keymap.set("n", "<leader>bd", "<cmd>%bd<CR>", { desc = "Close all buffers" })
 vim.keymap.set("n", "<leader>bo", "<cmd>%bd|e#|bd#<CR>", { desc = "Close all other buffers" })
