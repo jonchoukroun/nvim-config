20a55c1a346524ca817ebf2edee7ed249f589807
Jon Choukroun <jonchoukroun@gmail.com>
2025-05-17 17:04:35 -0700

    Configure tools installation with mason et al

---
+++ lua/config/plugins/mason.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup()
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"eslint",
+					"prettierd",
+					"stylelint",
+					"stylua",
+				},
+			})
+		end,
+	},
+}
