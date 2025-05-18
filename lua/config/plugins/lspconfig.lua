49389021bbdbe1925e73889dbcd5e6b205c58844
Jon Choukroun <jonchoukroun@gmail.com>
2025-05-17 17:01:16 -0700

    Disable nvim-lspconfig in favor of built-in nvim lsp

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -1,79 +1,79 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"emmet_ls",
-					"lua_ls",
-					"ts_ls",
-				},
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"eslint",
-					"prettierd",
-					"stylelint",
-					"stylua",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "saghen/blink.cmp" },
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					local opts = { desc = "", buffer = ev.buf }
-
-					opts.desc = "Go to definition"
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-
-					opts.desc = "Go to declaration"
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-					opts.desc = "Go to implementation"
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-
-					opts.desc = "Rename"
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				end,
-			})
-
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-
-			local lspconfig = require("lspconfig")
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-			lspconfig.clangd.setup({})
-			lspconfig.emmet_ls.setup({})
-			lspconfig.lua_ls.setup({
-				setings = { Lua = {} },
-			})
-			local sktCapabilities = capabilities
-			sktCapabilities.workspace = {
-				didChangeWatchedFiles = {
-					dynamicRegistration = true,
-				},
-			}
-			lspconfig.sourcekit.setup({
-				capabilities = sktCapabilities,
-			})
-			lspconfig.ts_ls.setup({})
-		end,
-	},
+	-- {
+	-- 	"williamboman/mason.nvim",
+	-- 	config = function()
+	-- 		require("mason").setup()
+	-- 	end,
+	-- },
+	-- {
+	-- 	"williamboman/mason-lspconfig.nvim",
+	-- 	config = function()
+	-- 		require("mason-lspconfig").setup({
+	-- 			ensure_installed = {
+	-- 				"emmet_ls",
+	-- 				"lua_ls",
+	-- 				"ts_ls",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	-- 	config = function()
+	-- 		require("mason-tool-installer").setup({
+	-- 			ensure_installed = {
+	-- 				"eslint",
+	-- 				"prettierd",
+	-- 				"stylelint",
+	-- 				"stylua",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	dependencies = { "saghen/blink.cmp" },
+	-- 	event = { "BufReadPre", "BufNewFile" },
+	-- 	config = function()
+	-- 		vim.api.nvim_create_autocmd("LspAttach", {
+	-- 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	-- 			callback = function(ev)
+	-- 				local opts = { desc = "", buffer = ev.buf }
+	--
+	-- 				opts.desc = "Go to definition"
+	-- 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	--
+	-- 				opts.desc = "Go to declaration"
+	-- 				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	--
+	-- 				opts.desc = "Go to implementation"
+	-- 				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	--
+	-- 				opts.desc = "Rename"
+	-- 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+	-- 			end,
+	-- 		})
+	--
+	-- 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+	--
+	-- 		local lspconfig = require("lspconfig")
+	-- 		local capabilities = require("blink.cmp").get_lsp_capabilities()
+	--
+	-- 		lspconfig.clangd.setup({})
+	-- 		lspconfig.emmet_ls.setup({})
+	-- 		lspconfig.lua_ls.setup({
+	-- 			setings = { Lua = {} },
+	-- 		})
+	-- 		local sktCapabilities = capabilities
+	-- 		sktCapabilities.workspace = {
+	-- 			didChangeWatchedFiles = {
+	-- 				dynamicRegistration = true,
+	-- 			},
+	-- 		}
+	-- 		lspconfig.sourcekit.setup({
+	-- 			capabilities = sktCapabilities,
+	-- 		})
+	-- 		lspconfig.ts_ls.setup({})
+	-- 	end,
+	-- },
 }
