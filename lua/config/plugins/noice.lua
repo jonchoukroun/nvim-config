fddc0824f28be81babf9e5020454f279834eaa31
Jon Choukroun <jonchoukroun@gmail.com>
2025-08-13 14:35:12 -0700

    Improve notification config

--- lua/config/plugins/noice.lua
+++ lua/config/plugins/noice.lua
@@ -1,31 +1,29 @@
 return {
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("noice").setup({
-				lsp = {
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("noice").setup({
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						kind = "",
+						find = "written",
 					},
+					opts = { skip = true },
 				},
-				routes = {
-					{
-						filter = {
-							event = "msg_show",
-							kind = "",
-							find = "written",
-						},
-						opts = { skip = true },
-					},
-				},
-				presets = {
-					command_palette = true,
-					long_message_to_split = true,
-				},
-			})
-		end,
-	},
+			},
+			presets = {
+				command_palette = true,
+				long_message_to_split = true,
+			},
+		})
+	end,
 }
--- lua/config/plugins/nvim-notify.lua
+++ lua/config/plugins/nvim-notify.lua
@@ -2,9 +2,10 @@ return {
 	"rcarriga/nvim-notify",
 	config = function()
 		require("notify").setup({
-			stages = "static",
-			render = "wrapped-compact",
+			stages = "no_animation",
+			render = "compact",
 			top_down = false,
+			merge_duplicates = 2,
 		})
 	end,
 }
