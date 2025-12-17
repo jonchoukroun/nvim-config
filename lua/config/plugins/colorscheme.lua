c05eb9c84138cc26e42b88603752c55a97cbd368
Jon Choukroun <jonchoukroun@gmail.com>
2025-12-17 10:42:25 -0800

    Add colorscheme options; go with neovim default

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,7 +1,29 @@
 return {
-	"jonchoukroun/nvim-phosphor",
-	name = "phosphor",
-	config = function()
-		vim.cmd("colorscheme phosphor")
-	end,
+	-- Updated legacy vim colorscheme
+	{
+		-- dir = "/Users/jonchoukroun/Developer/NvimPlugins/AphexColor/",
+		-- name = "aphex",
+		-- config = function()
+		-- 	require("aphex").setup({
+		-- 		-- Start with default settings
+		-- 	})
+		-- 	vim.cmd("colorscheme aphex")
+		-- end,
+	},
+	-- Phospohorescent blues and greens, orange accents
+	{
+		-- "jonchoukroun/nvim-phosphor",
+		-- name = "phosphor",
+		-- config = function()
+		-- 	vim.cmd("colorscheme phosphor")
+		-- end,
+	},
+	-- Stormy coastline, easy and pleasant
+	{
+		-- dir = "/Users/jonchoukroun/Developer/NvimPlugins/SimpleColor/",
+		-- name = "retrocode",
+		-- config = function()
+		-- 	vim.cmd("colorscheme retrocode")
+		-- end,
+	},
 }
