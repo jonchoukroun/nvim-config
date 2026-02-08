0a89fc9eb2443f539280a4813361fc278dc2f4bd
Jon Choukroun <jonchoukroun@gmail.com>
2026-02-07 21:47:54 -0800

    Remove previous structure

--- lua/config/lsp/sourcekit.lua
+++
@@ -1,6 +0,0 @@
-vim.lsp.config.sourcekit = {
-	cmd = { "sourcekit-lsp" },
-	filetypes = { "swift", "objc", "objcpp", "c", "cpp" },
-	root_markers = { ".git", "*.xcodeproj" },
-}
-vim.lsp.enable("sourcekit")
