1e88a8e1cc1219be1a5cad3236e78f3b313ef349
Jon Choukroun <jonchoukroun@gmail.com>
2026-03-19 07:08:31 -0700

    Disable luacheck

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,26 +1,26 @@
 return {
-    "mfussenegger/nvim-lint",
-    config = function ()
-        local lint = require("lint")
-        lint.linters_by_ft = {
-            lua = { "luacheck" },
-            css = { "stylelint" },
-            scss = { "stylelint" },
-            javascript = { "stylelint", "eslint_d" },
-            typescript = { "stylelint", "eslint_d" },
-            javascriptreact = { "eslint_d" },
-            typescriptreact = { "eslint_d" }
-        }
+	"mfussenegger/nvim-lint",
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			-- lua = { "luacheck" },
+			css = { "stylelint" },
+			scss = { "stylelint" },
+			javascript = { "stylelint", "eslint_d" },
+			typescript = { "stylelint", "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+		}
 
-        lint.linters.luacheck.args = {
-            "--globals",
-            "vim"
-        }
+		lint.linters.luacheck.args = {
+			"--globals",
+			"vim",
+		}
 
-        vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
-            callback = function ()
-                require("lint").try_lint()
-            end
-        })
-    end
+		vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+	end,
 }
