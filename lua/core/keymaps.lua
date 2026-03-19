117f033dd8eaac91674071ca3480f66ca0bc8063
Jon Choukroun <jonchoukroun@gmail.com>
2026-03-19 06:56:28 -0700

    Force close buffers to silence warning

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -20,7 +20,7 @@ setKeymap("n", "<C-u>", "<C-u>zz", { noremap = true })
 
 -- Buffer management
 setKeymap("n", "<leader>bd", "<cmd>%bd<CR>", { desc = "Close all buffers" })
-setKeymap("n", "<leader>bo", "<cmd>%bd|e#|bd#<CR>", { desc = "Close all other buffers" })
+setKeymap("n", "<leader>bo", "<cmd>%bd!|e#|bd#<CR>", { desc = "Close all other buffers" })
 
 -- Buffer settings
 setKeymap("n", "<leader>w", "<cmd>set wrap<CR>", { desc = "Wrap text on" })
