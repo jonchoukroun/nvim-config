b35ed7649273b6f62ca734cccec44e4bad226fc6
Jon Choukroun <jonchoukroun@gmail.com>
2026-03-23 15:05:26 -0700

    Formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -42,7 +42,7 @@ return {
 				vim.treesitter.start()
 				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 				vim.wo[0][0].foldmethod = "expr"
-                vim.wo[0][0].foldenable = false
+				vim.wo[0][0].foldenable = false
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 			end,
 		})
