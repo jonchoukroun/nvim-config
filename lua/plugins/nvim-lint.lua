89b4da164a98b322ef28a147d81564f44a13fb64
Jon Choukroun <jonchoukroun@gmail.com>
2026-03-23 17:11:06 -0700

    Improve config

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -2,14 +2,15 @@ return {
 	"mfussenegger/nvim-lint",
 	config = function()
 		local lint = require("lint")
+        vim.env.ESLINT_D_PPID = vim.fn.getpid()
 		lint.linters_by_ft = {
-			-- lua = { "luacheck" },
 			css = { "stylelint" },
-			scss = { "stylelint" },
 			javascript = { "stylelint", "eslint_d" },
+			javascriptreact = { "stylelint", "eslint_d" },
+            lua = { "luacheck" },
+			scss = { "stylelint" },
 			typescript = { "stylelint", "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
+			typescriptreact = { "stylelint", "eslint_d" },
 		}
 
 		lint.linters.luacheck.args = {
@@ -17,6 +18,7 @@ return {
 			"vim",
 		}
 
+        -- Will not lint on file open
 		vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
 			callback = function()
 				require("lint").try_lint()
