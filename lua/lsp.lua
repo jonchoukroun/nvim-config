8aad1c0329e6d28189095597ee15ca503bde1d73
Jon Choukroun <jonchoukroun@gmail.com>
2026-04-06 18:15:13 -0700

    Remove lua formatter from lsp confg

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,7 +2,6 @@ vim.lsp.enable({
 	"clangd",
 	"emmet-language-server",
 	"lua-language-server",
-	-- "stylua",
 	"typescript-language-server",
 })
 vim.diagnostic.config({ virtual_lines = { current_line = true } })
